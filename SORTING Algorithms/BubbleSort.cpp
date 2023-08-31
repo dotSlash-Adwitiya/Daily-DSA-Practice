69f55fa3e00093c6c4e0b410eaa11b304003844e
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-08-31 16:35:50 +0530

    Added Bubble Sort Algo

---
+++ SORTING Algorithms/BubbleSort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+  //* We have to perform n-1 passes so we take i to n-1 outer loop
+  for(int i = 0; i < n-1; i++) {
+      //* In each pass, we have to reduce comparisons based on no of elements 
+      //* That are already sorted. So, in each pass we sort i-elements. hence n-i-1
+      for(int j = 0; j < n-i-1; j++) 
+          if(arr[j] > arr[j+1])
+              swap(arr[j], arr[j+1]);
+  }
+}
+
+int main()
+{
+  int arr [] = {8,5,7,3,2};
+  int n = sizeof(arr)/sizeof(arr[0]);
+
+  cout << "Array before performing sorting : " << endl;
+  
+  for(int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+  
+  cout << endl;
+
+  bubbleSort(arr,n);
+
+  cout << "Array after performing sorting : " << endl;
+  for(int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+  
+  return 0;
+}
---
+++ SORTING Algorithms/TEMPLATE.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int arr [] = {8,5,7,3,2};
+  int n = sizeof(arr)/sizeof(arr[0]);
+
+  
+
+  return 0;
+}
