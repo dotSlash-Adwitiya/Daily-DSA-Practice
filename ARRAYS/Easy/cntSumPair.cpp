8d960ce1755c702c388c2836844e10937ecb3ce3
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-02 21:20:37 +0530

    Added pair sum & heap deletion code

--- ARRAYS/Easy/cntSumPair.cpp
+++ ARRAYS/Easy/cntSumPair.cpp
@@ -15,11 +15,40 @@ int getPairsCount(int arr[], int n, int k) {
 
 //* Optimized ->> TC : O(nlogn), SC : O(1)
 //* ------->>>>>> Using SORTING <<<<<<<-----------
-
+//* Only works if elements in given array are unique
+int getPairsCount(int arr[], int n, int k) {
+    int cnt = 0, i = 0, j = n-1;
+    sort(arr, arr+n);
+    
+    while(i < j) {
+        int sum = arr[i] + arr[j];
+        if(sum == k) {
+            cnt++;
+            i++;
+            j--;
+        }
+        else if(sum < k) i++;
+        else j--;
+    } 
+    return cnt;
+}
 
 
 //* Best Approach --> TC : O(n), SC : O(n)
 //* ---------->>>>>  Using HASH MAP <<<<<<<----------------
+int getPairsCount(int arr[], int n, int k) {
+    int cnt = 0;
+    unordered_map<int, int> ump;
+    
+    for(int i = 0; i < n; i++) {
+        //* Every time we find an element in array, we need to 
+        //* add the count, along with the previous counts, so we basically add up all the frequencies
+        if(ump[k-arr[i]] != 0) 
+            cnt = cnt + ump[k-arr[i]];
+        ump[arr[i]]++;
+    }
+    return cnt;
+}
 
 
 
