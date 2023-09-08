be069c2fb856a94f6f95f458c1b039dcbf95e0b0
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-08 23:59:11 +0530

    Create a Binary Search Tree (BST) from a given BST

---
+++ TREE/createBSTFromBT.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public : 
+    Node* left;
+    int data;
+    Node* right;
+  public : 
+    Node(int data) {
+      this->left = NULL;
+      this->data = data;
+      this->right = NULL;
+    }
+  friend class Tree;
+};
+
+class Tree {
+  public :
+};
+
+void createBST(Node* &root, priority_queue<int> &pq) {
+        if(!root) return;
+        
+        createBST(root->right, pq);
+        root->data = pq.top();
+        pq.pop();
+        createBST(root->left, pq);
+    }
+    
+    void traverse(Node* root, priority_queue<int> &pq) {
+        if(!root) return;
+        
+        traverse(root->left, pq);
+        pq.push(root->data);
+        traverse(root->right, pq);
+    }
+    
+    Node *binaryTreeToBST (Node *root)
+    {
+        priority_queue<int> pq;
+        traverse(root, pq);
+        createBST(root, pq);
+        return root;
+    }
+
+int main()
+{
+
+
+  return 0;
+}
