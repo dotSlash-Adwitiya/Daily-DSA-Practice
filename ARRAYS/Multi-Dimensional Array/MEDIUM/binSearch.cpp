e11cae6409841f1682e20168ef53519c30fb5207
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-24 21:01:17 +0530

    O(logN) + (LogM) Approach Added (More Optimised)

--- ARRAYS/Multi-Dimensional Array/MEDIUM/binSearch.cpp
+++ ARRAYS/Multi-Dimensional Array/MEDIUM/binSearch.cpp
@@ -9,6 +9,7 @@ using namespace std;
 // * We need to figure our how many row are left behind, along with additional elements to reach the current index.
 // * Note that, 1st element of every row will be a multiple of m. Where m = columns in our MATRIX. 
 
+// * TC " O(log m*n)"
 //* To only check if the element exists.
 bool binSearch(vector<vector<int>> &matrix, int key) {
 
@@ -52,6 +53,50 @@ vector<int> binSearchIdx(vector<vector<int>> &matrix, int key) {
   return {-1,-1};
 }
 
+bool binSearch1DArray(vector<int> &arr, int key , int n) {
+  //* n = column size of 2D array. 
+  //* Becoz, every row is treated as a single 1D Array.
+  int low = 0;
+  int high = n-1;
+
+  while(low <= high){
+    int mid = low + (high - low) / 2;
+    if(arr[mid] == key)
+      return true;
+    else if(arr[mid] < key)
+      low = mid + 1;
+    else 
+      high = mid - 1;
+  }
+  return false;
+}
+
+// * More Optimisde Approach : O((log M) +(log N))
+bool binSearchOptimised(vector<vector<int>> &matrix, int key) {
+  
+  int row = matrix.size();
+  int col = matrix[0].size();
+  int low = 0;
+  int high = col - 1;
+
+  while(low <= high) {
+    int mid = low +(high - low) / 2;
+    
+    //* Simply check if the element lies in current ROW
+    // * If yes, search in this row. Else apply binary search on remaining rows
+    if(key >= matrix[mid][0] && key <= matrix[mid][col-1])
+      return binSearch1DArray(matrix[mid], key, col);
+
+    else if(matrix[mid][0] < key)
+      low = mid + 1;
+    
+    else 
+      high = mid - 1;
+  }
+  return false;
+}
+
+
 int main()
 {
   vector<vector<int>> matrix {{1,2,3},
@@ -60,7 +105,9 @@ int main()
 
   vector<int> ans = binSearchIdx(matrix, 4);
 
-  cout << "[" << ans[0] << "]" << "[" << ans[1] << "]" << endl;
+  // cout << "[" << ans[0] << "]" << "[" << ans[1] << "]" << endl;
 
+  // * Optimised Bin Search Call : -
+  cout << "Optimised Bin Search : " << binSearchOptimised(matrix, 9) << endl;
   return 0;
 }
