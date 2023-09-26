1064e92c34eeecd84aa0670bfa62edfffe31dbcb
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-26 16:39:57 +0530

    Tabulation + Space optimisation on Tabulation Approach added

--- Dynamic Programming/fibonacciSeries.cpp
+++ Dynamic Programming/fibonacciSeries.cpp
@@ -21,8 +21,44 @@ int fibonacci(int n) {
   return fib(n, dp);
 }
 
+//* Iterative solution : Tabulation method.
+
+int fibonacciTab(int n) {
+  vector<int> dp(n+1);
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for(int i = 2; i <= n; i++) 
+    dp[i] = dp[i-1] + dp[i-2];
+
+  return dp[n];
+}
+
+// * Space optimisation : Intuition - We only need previous 2 numbers, 
+// * and we don't care about the rest.
+
+int fibonacci3(int n) {
+  
+  if(n == 1)
+    return 0;
+  if(n == 2)
+    return 1;
+  
+  int prev1 = 0;
+  int prev2 = 1;
+  
+  int curr = 0;
+
+  for(int i = 2; i <= n; i++) {
+    curr = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = curr;
+  }
+  return curr;
+}
+
 int main()
 {
-  cout << fibonacci(7) << endl;
+  cout << fibonacci3(7) << endl;
   return 0;
 }
