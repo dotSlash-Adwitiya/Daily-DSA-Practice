74468bfe47667a941059157795c0c80c7e0f635d
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-10-03 17:14:22 +0530

    TC : O(n), SC : O(1)

--- Strings/reverseWordsIII.cpp
+++ Strings/reverseWordsIII.cpp
@@ -19,3 +19,21 @@ string reverseWords(string s) {
   ans += temp;
   return ans;
 }
+
+string reverseWordsII(string s){
+  int n = s.size();
+  int i = 0, j = 0, k = 0;
+
+
+  while(j < n) {
+    while(j < n && s[j] != ' ') j++;
+    k = j - 1;
+
+    while(k > i)
+      swap(s[i++], s[k--]); 
+
+    i = j + 1;
+    j++;
+  }
+  return s;
+}
