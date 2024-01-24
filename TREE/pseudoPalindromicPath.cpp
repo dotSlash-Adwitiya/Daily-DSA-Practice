27e61f47233f7d8857b1d8cf8eac4d391fb39135
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-01-24 17:45:11 +0530

    Added HINTS

--- TREE/pseudoPalindromicPath.cpp
+++ TREE/pseudoPalindromicPath.cpp
@@ -1,3 +1,13 @@
+/*
+ * Given a binary tree where node values are digits from 1 to 9.
+ * A path in the binary tree is said to be pseudo-palindromic 
+ * if at least one permutation of the node values in the path is a palindrome.
+ * Return the number of pseudo-palindromic paths going 
+ * from the root node to leaf nodes.
+ *
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,20 +18,28 @@ struct Node
   Node* right;
 };
 
+//* HINT --->> You only need to figure out when a palindrome can be made ??
+//* PATH : Definition of path is ----->>  ****ROOT to LEAF****
+//* CASE : There can be at most 1 digit with odd frequency.
 
 void helper(Node* root, vector<int> &freq, int &cnt) {
   if(root == NULL)
   return;
 
+  //* As soon as we reach leaf, this is called a PATH.
   if(root->left == NULL && root->right == NULL) {
       
       freq[root->val]++;
 
+      // * Check if some element exists of odd frequency.
       int oddParity = 0;
       for(int i = 0; i < 10; i++) {
           if(freq[i] & 1)
-          oddParity++;
+            oddParity++;
       }
+
+      // * If there's single or no element of odd frequency, then 
+      // * there definetly exists a PATH !
       if(oddParity < 2)
           cnt++;
 
