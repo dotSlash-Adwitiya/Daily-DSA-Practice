68eafb7a9f1c1c51d4ed8d3103aa846f9f4f5391
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-06-06 23:02:23 +0530

    Q5. Detect a cycle in a graph using BFS

--- GRAPH/MEDIUM/detectCycleUndirectedBFS.cpp
+++ GRAPH/MEDIUM/detectCycleUndirectedBFS.cpp
@@ -1,44 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool detect(int src, vector<int> adj[], int vis[]) {
-  vis[src] = 1; 
-  // * store <source node, parent node>
-  // * Initial Configuration
-  queue<pair<int,int>> q; 
-  q.push({src, -1}); 
-
-  while(!q.empty()) {
-      int node = q.front().first; 
-      int parent = q.front().second; 
-      q.pop(); 
-      
-      // Explore all adjacent nodes
-      for(auto adjacentNode: adj[node]) {
-          // if adjacent node is unvisited
-          if(!vis[adjacentNode]) {
-              vis[adjacentNode] = 1; 
-              q.push({adjacentNode, node}); 
-          }
-
-          // * if adjacent node is visited and is not it's own parent node
-          else if(parent != adjacentNode) {
-              return true; // * yes it is a cycle
-          }
-      }
-}
-  // * No cycle
-  return false; 
+bool detect(int src, vector<int> adj[], vector<int> &vis){
+    // * Below is initial Configuration
+    vis[src] = 1;
+    queue<pair<int,int>> q;
+    q.push({src, -1});
+    
+    // * BFS Start
+    while(!q.empty()){
+        int node = q.front().first;
+        int parentNode = q.front().second;
+        q.pop();
+        
+        for(auto adjNode : adj[node]){
+            if(!vis[adjNode]){
+                vis[adjNode] = 1;
+                q.push({adjNode, node});
+            }else if(adjNode != parentNode) {
+                return true;
+            }
+        }
+    }
+    // * BFS END
+    return false;
 }
 
-// Function to detect cycle in an undirected graph.
+
 bool isCycle(int V, vector<int> adj[]) {
-  // initialise them as unvisited 
-  int vis[V] = {0};
-  for(int i = 0;i<V;i++) {
-      if(!vis[i]) {
-          if(detect(i, adj, vis)) return true; 
-      }
-  }
-  return false; 
+    vector<int> vis(V,0);
+
+    // * We need to iterate over every COMPONENT
+    // * Thats why we use a for loop here!
+    for(int i = 0; i < V; i++) 
+        if(!vis[i] && detect(i, adj, vis))
+            return true;
+    
+    return false;    
 }
