85a0655ced3eb6697db97a6fb4ccb322a330e3c4
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-06-12 13:38:47 +0530

    Practiced

--- GRAPH/MEDIUM/detectCycleUndirectedDFS.cpp
+++ GRAPH/MEDIUM/detectCycleUndirectedDFS.cpp
@@ -27,3 +27,37 @@ bool isCycle(int V, vector<int> adj[]) {
   }
   return false; 
 }
+
+// * Practice session - 1 :
+
+bool dfs(vector<int> adj[], int vis[], int parentNode, int currNode) {
+  vis[currNode] = 1;
+  
+  for(int ele : adj[currNode]){
+      if(vis[ele] && ele != parentNode)
+          return true;
+          
+      else if(vis[ele] && ele == parentNode)
+          continue;
+          
+      else {
+          if(dfs(adj, vis, currNode, ele))
+              return true;
+      }
+  }
+  return false;
+}
+
+bool isCycle(int V, vector<int> adj[]) {
+  int vis[V+1] = {0};
+  vis[0] = 1;
+
+    for(int i = 0; i < V; i++) {
+      if(vis[i] == 0) {
+          if(dfs(adj, vis, -1, i))
+              return true;
+      }
+  }
+  return false;
+
+}
