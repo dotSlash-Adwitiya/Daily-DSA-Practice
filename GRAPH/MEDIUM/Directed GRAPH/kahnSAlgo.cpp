0b489a9d775ce5e071ac9e746a9a9532974bedee
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-06-20 15:36:55 +0530

    Q12. Kahn's Algo for Finding Topological Sort using BFS

--- GRAPH/MEDIUM/Directed GRAPH/kahnSAlgo.cpp
+++ GRAPH/MEDIUM/Directed GRAPH/kahnSAlgo.cpp
@@ -4,19 +4,24 @@ using namespace std;
 vector<int> topoSort(int V, vector<int> adj[])
 {
   int indegree[V] = {0};
-  for (int i = 0; i < V; i++) {
-    for (auto it : adj[i]) {
+
+  // * Count and STORE the indegree of every node
+  for (int i = 0; i < V; i++) { // * Traversing on Each Vertex
+    for (auto it : adj[i]) { // * If an element is found at any node, increase its indegree
       indegree[it]++;
     }
   }
 
   queue<int> q;
+  // * Put all the nodes with 0 incoming edge (indegree) in a queue
   for (int i = 0; i < V; i++) {
     if (indegree[i] == 0) {
       q.push(i);
     }
   }
+
   vector<int> topo;
+
   while (!q.empty()) {
     int node = q.front();
     q.pop();
@@ -26,7 +31,7 @@ vector<int> topoSort(int V, vector<int> adj[])
     // * so please remove it from the indegree
 
     for (auto it : adj[node]) {
-      indegree[it]--;
+      indegree[it]--; // * Decrement the outdegree
       if (indegree[it] == 0) q.push(it);
     }
   }
