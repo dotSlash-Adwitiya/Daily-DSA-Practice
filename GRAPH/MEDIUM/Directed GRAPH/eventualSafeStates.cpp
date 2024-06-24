038d68e0f52400a6eb1a335164fd8b96f6b38e3e
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-06-24 20:06:40 +0530

    2nd Approach for finding safe nodes using TOPOSORT Logic

--- GRAPH/MEDIUM/Directed GRAPH/eventualSafeStates.cpp
+++ GRAPH/MEDIUM/Directed GRAPH/eventualSafeStates.cpp
@@ -40,3 +40,44 @@ vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
   
   return safeNodes;
 }
+
+
+// * 2nd Approach: Using TOPOSort logic
+// * Step-1: Reverse the GRAPH (Ex: If there exists an edge from ele1->ele2, then make it: ele2->ele1)
+vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+    vector<int> adjRev[V];
+    int indegree[V] = {0};
+    
+    for(int i = 0; i < V; i++) {
+        // * Current there exists an edge b/w : i-> ele
+        // * Convert it to: ele->i
+        for(int ele: adj[i]){
+            adjRev[ele].push_back(i);
+            indegree[i]++; // * While reversing, count the no of incoming nodes
+        }
+    }
+    
+    queue<int> q;
+    for(int i = 0; i < V; i++) {
+        if(indegree[i] == 0)
+            q.push(i);
+    }
+    
+    vector<int> safenodes;
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        // * We can pussh this node because, we know whichever node has an indegree == 0
+        // * will ONLY be present in the QUEUE Data Structure
+        safenodes.push_back(node);
+        for(int &ele: adjRev[node]){
+            indegree[ele]--;
+            if(indegree[ele] == 0)
+                q.push(ele);
+        }
+        
+    }
+    
+    sort(safenodes.begin(), safenodes.end());
+    return safenodes;
+}
