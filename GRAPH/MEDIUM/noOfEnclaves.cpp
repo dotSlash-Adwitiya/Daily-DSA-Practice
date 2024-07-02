a2a49c64941a4df6006a88ab48bbe928ccab60d1
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-02 23:23:40 +0530

    DFS Approach, boundary Traversal

--- GRAPH/MEDIUM/noOfEnclaves.cpp
+++ GRAPH/MEDIUM/noOfEnclaves.cpp
@@ -101,3 +101,52 @@ int numberOfEnclaves(vector<vector<int>> &grid) {
     }
     return enclaves;
 }
+
+void dfs(int row, int col, vector<vector<int>> &grid, vector<vector<int>> &vis, int n, int m) {
+    
+    if(row >= n || row < 0 || col < 0 || col >= m || grid[row][col] == 0)
+        return;
+        
+    int delRow[] = {-1, 0, +1, 0};
+    int delCol[] = {0, +1, 0, -1};
+    
+    // * MARK The current Cell as 0
+    grid[row][col] = 0;
+    
+    for(int i = 0; i < 4; i++) {
+        int nrow = row + delRow[i];
+        int ncol = col + delCol[i];
+        
+        dfs(nrow, ncol, grid, vis, n, m);
+    }
+
+}
+
+// * 2nd Approach using DFS    
+int numberOfEnclaves(vector<vector<int>> &grid) {
+    // * Initial Config START
+    // * STEP - 1: Create a VIS matrix
+    int n = grid.size(), m = grid[0].size();
+    vector<vector<int>> vis(n, vector<int> (m, 0));
+    // * Initial Config END
+    
+    // * STEP - 2: Mark all boundary lands as visited recursively using DFS
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+                if(grid[i][j] == 1 && !vis[i][j]){ //  * if this is a BOUNDARY LAND which is not visited
+                    dfs(i, j, grid, vis, n, m);
+                }
+            }
+        }
+    }
+    
+    int enclaves = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(grid[i][j] && !vis[i][j])
+                enclaves++;
+        }
+    }
+    return enclaves;
+}
