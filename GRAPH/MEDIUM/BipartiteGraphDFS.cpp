6ed3019881314cfbe5a63c2762a864f50adaed26
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-04 23:35:52 +0530

    Practice - 1

--- GRAPH/MEDIUM/BipartiteGraphDFS.cpp
+++ GRAPH/MEDIUM/BipartiteGraphDFS.cpp
@@ -24,3 +24,30 @@ bool isBipartite(int V, vector<int>adj[]){
   }
   return true;
 }
+
+// * PRACTICE
+bool dfs(int node, vector<int>adj[], vector<int> &vis, int color){
+    vis[node] = color;
+    
+    for(auto &ele : adj[node]){
+        if(vis[ele] == -1){
+            vis[ele] = !vis[node];
+            if(!dfs(ele, adj, vis, !color))
+                return false;
+        }
+        else if(vis[ele] == color)
+            return false;
+    }
+    return true;
+}
+
+bool isBipartite(int V, vector<int>adj[]){
+    vector<int> vis(V, -1);
+    for(int i = 0; i < V; i++){
+        if(vis[i] == -1) {
+            if(!dfs(i, adj, vis, 0))
+                return false;
+        }
+    }
+    return true;
+}
