8f6de57ea45a3196797aa04190aeeb4ef13903af
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-04 13:53:05 +0530

    Check identical

--- TREE/isIdentical.cpp
+++ TREE/isIdentical.cpp
@@ -36,3 +36,19 @@ bool isIdentical(Node *r1, Node *r2)
 { 
     return ((r1 == NULL && r2 == NULL) ? true :(((r1 == NULL && r2 != NULL) || (r2 == NULL && r1 != NULL)) ? false : (r1->data == r2->data && isIdentical(r1->left, r2->left) && isIdentical(r1->right, r2->right))));
 }
+
+// * Practice
+bool isIdentical(Node *r1, Node *r2)
+{
+    if(!r1 && !r2)
+        return true;
+    if((!r1 && r2) || (r1 && !r2))
+        return false;
+    if (r1->data != r2->data)
+        return false;
+        
+    bool left = isIdentical(r1->left, r2->left);  
+    bool right = isIdentical(r1->right, r2->right);
+    
+    return left and right;
+}
