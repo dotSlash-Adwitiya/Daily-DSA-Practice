13939d1133f032a2320880228b656ebcff870af3
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-11 00:13:01 +0530

    Q.36 Graph

--- GRAPH/MEDIUM/ShortestPathInMatrix.cpp
+++ GRAPH/MEDIUM/ShortestPathInMatrix.cpp
@@ -3,36 +3,40 @@ using namespace std;
 
 int shortestPath(vector<vector<int>> &grid, pair<int, int> source, pair<int, int> destination) {
   queue<pair<int, pair<int, int>>> q;
-  int n = grid.size(), m = grid[0].size();
 
-  vector<vector<int>> dist(n, vector<int>(m, 1e9));
-  dist[source.first][source.second] = 0;
-  q.push({0, {source.first, source.second}});
+    if(source.first==destination.first && source.second==destination.second) 
+        return 0;  
 
-  int drow[] = {-1, 0, +1, 0};
-  int dcol[] = {0, +1, 0, -1};
+    int n = grid.size(), m = grid[0].size();
 
-  while (!q.empty()) {
-      int dis = q.front().first;
-      int row = q.front().second.first;
-      int col = q.front().second.second;
-      q.pop();
+    vector<vector<int>> dist(n, vector<int>(m, 1e9));
+    dist[source.first][source.second] = 0;
+    q.push({0, {source.first, source.second}});
 
-      for (int i = 0; i < 4; i++) {
-          int nrow = row + drow[i];
-          int ncol = col + dcol[i];
+    int drow[] = {-1, 0, +1, 0};
+    int dcol[] = {0, +1, 0, -1};
 
-          if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
-              && grid[nrow][ncol] == 1 && dis + 1 < dist[nrow][ncol]) {
-              dist[nrow][ncol] = dis + 1;
-              q.push({dis + 1, {nrow, ncol}});
+    while (!q.empty()) {
+        int dis = q.front().first;
+        int row = q.front().second.first;
+        int col = q.front().second.second;
+        q.pop();
 
-              if (nrow == destination.first && ncol == destination.second) {
-                  return dis + 1;
-              }
-          }
-      }
-  }
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
 
-  return -1;
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
+                && grid[nrow][ncol] == 1 && dis + 1 < dist[nrow][ncol]) {
+                dist[nrow][ncol] = dis + 1;
+                q.push({dis + 1, {nrow, ncol}});
+
+                if (nrow == destination.first && ncol == destination.second) {
+                    return dis + 1;
+                }
+            }
+        }
+    }
+
+    return -1;
 }
