0b9a1ee99f47c03305d028994dd2ab2e95b393d0
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-11 23:57:49 +0530

    Practice - 1

--- GRAPH/MEDIUM/Directed GRAPH/Dijkstra-PQ.cpp
+++ GRAPH/MEDIUM/Directed GRAPH/Dijkstra-PQ.cpp
@@ -35,3 +35,40 @@ vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
   }
   return dist;
 }
+
+//  *Practice - 1
+#include<bits/stdc++.h>
+using namespace std;
+
+vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+{
+  priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> pq;
+  vector<int> dist(V, 0);
+          
+  for(int i = 0; i < V; i++)
+      dist[i] = 1e9;
+  
+  // * Src will always be reachable at 0 distance
+  dist[S] = 0;
+  pq.push({0, S});
+  
+  
+  while(!pq.empty()) {
+
+      int nodeDist = pq.top().first;
+      int node = pq.top().second;
+      
+      pq.pop();
+      
+      for(auto it: adj[node]){
+          int edgeWt = it[1]; 
+          int adjNode = it[0];
+
+          if(nodeDist + edgeWt < dist[adjNode]){
+              dist[adjNode] = nodeDist + edgeWt;
+              pq.push({dist[adjNode], adjNode});
+          }
+      }
+  }
+  return dist;
+}
