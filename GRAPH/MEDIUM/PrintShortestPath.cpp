f27c47e31366af8ec358119d25c6c249146d5400
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-25 23:43:11 +0530

    Graph Dijkstra - PQ, Print PATH

---
+++ GRAPH/MEDIUM/PrintShortestPath.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
+  vector<pair<int, int>> adj[n+1];
+  
+  for(auto it : edges) {
+      adj[it[0]].push_back({it[1], it[2]});
+      adj[it[1]].push_back({it[0], it[2]});
+  }
+  
+  priority_queue<pair<int, int>,vector<pair<int,int> >, 
+  greater<pair<int,int>>> pq; 
+  
+  vector<int> dist(n+1, 1e9), parent(n+1);
+  
+
+  for(int i = 1; i <= n; i++) parent[i] = i;
+  dist[1] = 0; // * Src will be at 0 distance
+  
+  pq.push({0,1});
+  
+  while(!pq.empty()){
+      int nodeDist = pq.top().first;
+      int node = pq.top().second;
+      
+      pq.pop();
+      
+      for(auto it : adj[node]){
+          int adjNode = it.first;
+          int edgeWt = it.second;
+          
+          if(nodeDist + edgeWt < dist[adjNode]){
+              dist[adjNode] = nodeDist + edgeWt;
+              pq.push({dist[adjNode], adjNode});
+              
+              // * Store its parent -- THE ONLY MODIFICATION REQUIRED
+              parent[adjNode] = node;
+          }
+      }
+  }
+  if(dist[n] == 1e9) return {-1};
+
+  vector<int> path;
+  int node = n;
+  while(parent[node] != node) {
+      path.push_back(node);
+      node = parent[node];
+  }    
+  path.push_back(1);
+  // * Insert shortest path's COST(Total Weight to reach destination node) 
+  path.push_back(dist[n]);
+  reverse(path.begin(), path.end());
+  return path;
+}
