3465e96beb5fc64aa400857f0224c9ebe325cb62
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-07-26 23:41:14 +0530

    Practice - 1

--- GRAPH/MEDIUM/ShortestPathInMatrix.cpp
+++ GRAPH/MEDIUM/ShortestPathInMatrix.cpp
@@ -40,3 +40,44 @@ int shortestPath(vector<vector<int>> &grid, pair<int, int> source, pair<int, int
 
     return -1;
 }
+
+// * Practice - 1
+int shortestPath(vector<vector<int>> &grid, pair<int, int> source, pair<int, int> destination) {
+
+    if(source.first==destination.first && source.second==destination.second) 
+    return 0;
+    
+    queue<pair<int, pair<int, int>>> q;
+    int n = grid.size(), m = grid[0].size();
+
+    vector<vector<int>> dist(n, vector<int>(m, 1e9));
+    dist[source.first][source.second] = 0;
+    q.push({0, {source.first, source.second}});
+
+    int drow[] = {-1, 0, +1, 0};
+    int dcol[] = {0, +1, 0, -1};
+
+    while (!q.empty()) {
+        int dis = q.front().first;
+        int row = q.front().second.first;
+        int col = q.front().second.second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
+                && grid[nrow][ncol] == 1 && dis + 1 < dist[nrow][ncol]) {
+                dist[nrow][ncol] = dis + 1;
+                q.push({dis + 1, {nrow, ncol}});
+
+                if (nrow == destination.first && ncol == destination.second) {
+                    return dis + 1;
+                }
+            }
+        }
+    }
+
+    return -1;
+}
