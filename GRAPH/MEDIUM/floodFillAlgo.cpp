3e64dff4c933f727430216f491a98cb9a130d0b7
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-08-06 23:35:03 +0530

    Practice - 2

--- GRAPH/MEDIUM/floodFillAlgo.cpp
+++ GRAPH/MEDIUM/floodFillAlgo.cpp
@@ -12,6 +12,7 @@ void dfs(int row, int col, vector<vector<int>>&ans,
     for(int i = 0;i<4;i++) {
         int nrow = row + delRow[i]; 
         int ncol = col + delCol[i]; 
+        
         // check for valid coordinate 
         // then check for same initial color and unvisited pixel
         if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
@@ -84,4 +85,38 @@ vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int ne
 
     dfs(image, ans, newColor, initialColor, sr, sc); 
     return ans;
+}
+
+// * Practice - 2
+void dfs(int row, int col, vector<vector<int>>&ans,
+    vector<vector<int>>& image, int newColor, int delRow[], int delCol[],
+    int iniColor) {
+    // paint with new color
+    ans[row][col] = newColor; 
+    int n = image.size();
+    int m = image[0].size(); 
+    // there are exactly 4 neighbours
+    for(int i = 0;i<4;i++) {
+        int nrow = row + delRow[i]; 
+        int ncol = col + delCol[i]; 
+        // check for valid coordinate 
+        // then check for same initial color and unvisited pixel
+        if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
+        image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) {
+            dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor); 
+        }
+    }
+}
+
+vector<vector<int>> floodFill(vector<vector<int>>& image, 
+int sr, int sc, int newColor) {
+    // initial color configuration
+    int iniColor = image[sr][sc]; 
+    vector<vector<int>> ans = image; 
+
+    // delta row and delta column for neighbours
+    int delRow[] = {-1, 0, +1, 0};
+    int delCol[] = {0, +1, 0, -1}; 
+    dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor); 
+    return ans; 
 }
