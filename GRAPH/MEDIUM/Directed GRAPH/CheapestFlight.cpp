6da8ec47996fde3c11dab46d0ae179d703958c68
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-08-14 23:39:28 +0530

    Practice - 1

--- GRAPH/MEDIUM/Directed GRAPH/CheapestFlight.cpp
+++ GRAPH/MEDIUM/Directed GRAPH/CheapestFlight.cpp
@@ -1,11 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
-
-
 int CheapestFLight(int n, vector<vector<int>> &flights,
-                  int src, int dst, int K)
-{
+                  int src, int dst, int K) {
   vector<pair<int, int>> adj[n];
   for (auto it : flights)
       adj[it[0]].push_back({it[1], it[2]});
@@ -45,4 +42,42 @@ int CheapestFLight(int n, vector<vector<int>> &flights,
     // else return the calculated dist from src to dst.
       return -1;
   return dist[dst];
+}
+
+// * PRACTICE - 1
+int CheapestFLight(int n, vector<vector<int>> &flights,
+                       int src, int dst, int K) {
+    vector<pair<int, int>> adj[n];
+    for (auto it : flights)
+        adj[it[0]].push_back({it[1], it[2]});
+    
+    queue<pair<int, pair<int, int>>> q;
+    
+    q.push({0, {src, 0}});    
+    
+    vector<int> dist(n, 1e9);
+    dist[src] = 0;
+
+    while (!q.empty()) {
+        auto it = q.front();
+        q.pop();
+        int stops = it.first;
+        int node = it.second.first;
+        int cost = it.second.second;
+
+        if (stops > K)
+            continue;
+        for (auto iter : adj[node]) {
+            int adjNode = iter.first;
+            int edW = iter.second;
+
+             if (cost + edW < dist[adjNode] && stops <= K) {
+                dist[adjNode] = cost + edW;
+                q.push({stops + 1, {adjNode, cost + edW}});
+            }
+        }
+    }
+    if (dist[dst] == 1e9)
+        return -1;
+    return dist[dst];
 }
