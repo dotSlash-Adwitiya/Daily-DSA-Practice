a74821615f3218bf6e93965b4756eb482c6c1976
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-08-19 23:26:06 +0530

    Practice - 1

--- GRAPH/MEDIUM/WordLadder-II.cpp
+++ GRAPH/MEDIUM/WordLadder-II.cpp
@@ -80,3 +80,58 @@ vector<vector<string>> findSequences(string beginWord, string endWord,
   }
   return ans;
 }
+
+// * Practice - 1
+vector<vector<string>> findSequences(string beginWord, string endWord,
+                                         vector<string> &wordList) {
+  unordered_set<string> st(wordList.begin(), wordList.end());
+  
+  queue<vector<string>> q;
+  q.push({beginWord});
+
+  vector<string> usedOnLevel;
+  usedOnLevel.push_back(beginWord);
+  int level = 0;
+  
+  vector<vector<string>> ans;
+  while (!q.empty()) {
+      vector<string> vec = q.front();
+      q.pop();
+
+      if (vec.size() > level) {
+          level++;
+          for (auto it : usedOnLevel) {
+              st.erase(it);
+          }
+      }
+
+      string word = vec.back();
+
+      // store the answers if the end word matches with targetWord.
+      if (word == endWord) {
+          // the first sequence where we reached end
+          if (ans.size() == 0)
+            ans.push_back(vec);
+          
+          else if (ans[0].size() == vec.size())
+              ans.push_back(vec);
+      }
+      for (int i = 0; i < word.size(); i++) {   
+          char original = word[i];
+          for (char c = 'a'; c <= 'z'; c++) {
+              word[i] = c;
+              if (st.count(word) > 0) { 
+                  // Check if the word is present in the wordList and
+                  // push the word along with the new sequence in the queue.
+                  vec.push_back(word);
+                  q.push(vec);
+                  // mark as visited on the level
+                  usedOnLevel.push_back(word);
+                  vec.pop_back();
+              }
+          }
+          word[i] = original;
+      }
+  }
+  return ans;
+}
