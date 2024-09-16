dc054ccc2b98339cbdedb6e237dc83224902bba8
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-09-16 20:30:57 +0530

    Disjoint Set Notes added

---
+++ GRAPH/Disjoint Set/DisjointSetTheory.cpp
@@ -0,0 +1,9 @@
+/*
+  * Disjoint set DS allows us to check if two nodes in a graph 
+  ! & BELONG to the SAME COMPONENT in O(1) Constand TIME
+  * DISJOINT SET Use CASE:
+  * Dynamic Graphs (Graphs which keep on changing the configuration every moment)
+  * Functions() :
+  * findParent()
+  * unionOfGraph() - can be found in 2 ways: Rank and Size
+*/
